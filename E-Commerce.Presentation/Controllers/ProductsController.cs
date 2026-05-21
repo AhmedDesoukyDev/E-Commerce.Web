e819ce3a4e7fbfbb3bbd757370d232c5f1f9e7f6
ahmed <ahmed@DESKTOP-V7ATK6T>
2026-05-21 04:43:24 +0300

    Product Controller

---
+++ E-Commerce.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,54 @@
+﻿using E_Commerce.Services.Abstraction;
+using E_Commerce.Shared.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly IProductService _productService;
+
+		public ProductsController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+
+		[HttpGet]
+		// GET : BaseUrl/api/Products
+		public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAllProducts()
+		{
+			var products = await _productService.GetAllProductsAsync();
+			return Ok(products);
+		}
+		[HttpGet("types")]
+		//GET : BaseUrl/api/Products/types
+		public async Task<ActionResult<IEnumerable<TypeDTO>>> GetAllTypes()
+		{
+			var types = await _productService.GetAllTypesAsync();
+			return Ok(types);
+		}
+		[HttpGet("brands")]
+		//GET : BaseUrl/api/Products/brands
+
+		public async Task<ActionResult<IEnumerable<BrandDTO>>> GetAllBrands()
+		{
+			var brands = await _productService.GetAllBrandsAsync();
+			return Ok(brands);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ProductDTO>> GetProduct(int id) 
+		{
+			var product = await _productService.GetProductByIdAsync(id);
+			return Ok(product);
+		}
+	}
+}
--- E-Commerce.Web/Extensions/WebApplicationBuilderExtensions.cs
+++ E-Commerce.Web/Extensions/WebApplicationBuilderExtensions.cs
@@ -3,6 +3,8 @@ using E_Commerce.Domain.UnitOfWork;
 using E_Commerce.Persistence.Data.DataSeeding;
 using E_Commerce.Persistence.Data.DbContexts;
 using E_Commerce.Persistence.Data.UnitOfWork;
+using E_Commerce.Services;
+using E_Commerce.Services.Abstraction;
 using E_Commerce.Services.MappingProfiles;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,6 +26,7 @@ namespace E_Commerce.Web.Extensions
 			builder.Services.AddScoped<IDataInitializer, DataInitializer>();
 			builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 			builder.Services.AddAutoMapper(X => X.AddProfile<ProductProfile>());
+			builder.Services.AddScoped<IProductService, ProductService>();
 			return builder;
 
 		}
